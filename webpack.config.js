e4703bc95b61f775bba1219f0580ab4d31f8b7b3
Dimitri Koval <dkoval@protonmail.com>
2017-08-30 18:47:49 +0300

    Fix some js errors (#61)
    TODO:
    1. Add modernizr
    2. Fix ecommerce styles
    3. Fix ecommerce images

--- webpack.config.js
+++ webpack.config.js
@@ -1,122 +1,87 @@
-const path = require('path');
-const glob = require('glob');
-const webpack = require('webpack');
+const path = require('path')
+const glob = require('glob')
+const webpack = require('webpack')
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const config = {
-  entry: './javascripts/theme.js',
+var plugins = [
+  new ExtractTextPlugin({
+    filename: 'theme.css'
+  }),
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+    Tether: 'tether',
+    bootstrap: 'bootstrap'
+  }),
+  new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.bundle.js'})
+]
+
+glob.sync('library/{,ecommerce/,docs/,blog/}*.html').forEach((page) => {
+  var pageName = path.basename(page).replace('.html', '')
+
+  // 'index-app' -> 'Index App'
+  var title = pageName
+    .replace('-', ' ')
+    .replace(/\b\w/g, (l) => l.toUpperCase())
+
+  plugins.push(
+    new HtmlWebpackPlugin({
+      title: title,
+      pageName: pageName,
+      filename: path.relative('library', page),
+      inject: 'head',
+      template: path.resolve(__dirname, page)
+    })
+  )
+})
+
+module.exports = {
+  cache: true,
+  entry: {
+    js: './javascripts/theme.js',
+    vendor: ['bootstrap', 'jquery', 'tether']
+  },
   devServer: {
     contentBase: './public'
   },
   output: {
-    path: path.resolve(__dirname, 'public/'),
+    path: path.resolve(__dirname, 'public'),
     publicPath: '/',
     filename: 'theme.js'
   },
   module: {
-    loaders: [
-      {
-        test: /\.html$/,
-        loader: 'html-loader',
-        options: {
-          interpolate: true,
-          root: __dirname,
-          attrs: ['img:src', 'link:href']
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      },
-      {
-        test: /\.(ico|jpg|jpeg|gif|png)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[path][hash]-[name].[ext]',
-          limit: 25000
-        }
-      },
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-        options: { name: 'fonts/[name].[hash].[ext]' }
-      },
-      {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-        options: { name: 'fonts/[name].[hash].[ext]' }
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
-        options: { name: 'fonts/[name].[hash].[ext]' }
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader',
-        options: { name: 'fonts/[name].[hash].[ext]' }
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
-        options: { name: 'fonts/[name].[hash].[ext]' }
+    loaders: [{
+      test: /.html$/,
+      loader: 'html-loader',
+      options: {
+        interpolate: true,
+        root: path.resolve(__dirname),
+        attrs: [':src']
       }
-    ]
+    },
+    {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'sass-loader']
+      })
+    },
+    {
+      test: /\.(ico|jpg|jpeg|gif|png)$/,
+      loader: 'file-loader',
+      options: {
+        name: 'images/[path][hash]-[name].[ext]',
+        limit: 25000
+      }
+    },
+    {
+      test: /\.(woff2?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url-loader?limit=10000',
+      options: { name: 'fonts/[name].[hash].[ext]' }
+    }]
   },
-  plugins: [
-    new ExtractTextPlugin('theme.css'),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      "window.jQuery": "jquery",
-      Tether: 'tether'
-    }),
-  ]
-};
-
-glob.sync('library/*.html').forEach((page) => {
-  config.plugins.push(
-    new HtmlWebpackPlugin({
-      filename: path.basename(page),
-      template: page,
-      inject: 'head'
-    })
-  )
-});
-
-glob.sync('library/docs/*.html').forEach((page) => {
-  config.plugins.push(
-    new HtmlWebpackPlugin({
-      filename: path.join('docs', path.basename(page)),
-      template: page,
-      inject: 'head'
-    })
-  )
-});
-
-glob.sync('library/ecommerce/*.html').forEach((page) => {
-  config.plugins.push(
-    new HtmlWebpackPlugin({
-      filename: path.join('ecommerce', path.basename(page)),
-      template: page,
-      inject: 'head'
-    })
-  )
-});
-
-glob.sync('library/blog/*.html').forEach((page) => {
-  config.plugins.push(
-    new HtmlWebpackPlugin({
-      filename: path.basename(page),
-      template: page,
-      inject: 'head'
-    })
-  )
-});
-
-module.exports = config;
+  plugins: plugins
+}
